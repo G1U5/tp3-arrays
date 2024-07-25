33df5609772bbdc9bdf976a55376b47631fd1873
Isaías Gius <isaiasgius@outlook.es>
2024-07-25 14:03:32 -0300

    agregamos detalles de algunos ejercicios

--- Js/ejercicio5.js
+++ Js/ejercicio5.js
@@ -1,16 +1,16 @@
 function analisisCadena(cadena){
     if (cadena === cadena.toUpperCase()){
-        return 'La cadena tiene solo letras mayúsculas'
+        return 'tiene solo letras mayúsculas'
     }else if (cadena === cadena.toLowerCase()){
-        return 'La cadena tiene solo letras minúsculas'
+        return 'tiene solo letras minúsculas'
     }else{
-        return 'La cadena tiene mayúsculas y minúsculas'
+        return 'tiene mayúsculas y minúsculas'
     }
 }
 const cadena1 = 'HOLA MUNDO'
 const cadena2 = 'hola mundo'
 const cadena3 = 'hOlA MunDo'
 
-document.write(`<p>${analisisCadena(cadena1)}</p>`)
-document.write(`<p>${analisisCadena(cadena2)}</p>`)
-document.write(`<p>${analisisCadena(cadena3)}</p>`)
+document.write(`<p>la cadena: ${cadena1} ${analisisCadena(cadena1)}</p>`)
+document.write(`<p>la cadena: ${cadena2} ${analisisCadena(cadena2)}</p>`)
+document.write(`<p>la cadena: ${cadena3} ${analisisCadena(cadena3)}</p>`)
